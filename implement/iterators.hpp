f63750e928b7900f1adfd05ad9cf2759cc223669
Oussama Jamil <ojamil@e1r8p5.1337.ma>
2022-09-26 12:18:59 +0100

    ft_con

--- implement/iterators.hpp
+++ implement/iterators.hpp
@@ -1,21 +1,108 @@
-#ifndef ITERATORS
-#define ITERATORS
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   iterators.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ojamil <ojamil@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/25 02:34:19 by ojamil            #+#    #+#             */
+/*   Updated: 2022/09/25 04:37:49 by ojamil           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-class iterators
-{
-private:
-	/* data */
-public:
-	iterators(/* args */);
-	~iterators();
-};
 
-iterators::iterators(/* args */)
-{
+#ifndef ITERATORS_HPP
+#define ITERATORS_HPP
+
+#include "./iterators_traits.hpp"
+
+namespace ft{
+	template < class T>
+	class iterator{
+		private:
+			pointer data;
+		public:
+		typedef typename ft::iterator_traits<T>::difference_type  difference_type;
+		typedef typename ft::iterator_traits<T>::pointer  pointer;
+		typedef typename ft::iterator_traits<T>::reference  reference;
+		typedef typename ft::iterator_traits<T>::iterator_category  iterator_category;
+		typedef typename  T value_type;
+
+		iterator(){}
+		iterator(const iterator& data){
+			*this = data;
+		}
+		iterator(const iterator& data){
+			this = data.data;
+		}
+		iterator& operator= (const iterator& data)const{
+			if(this != data){
+				this->data = data.data
+			}
+			return *this;
+		}
+		~iterator(){}
+
+		iterator& operator++ ()const{
+			this->data++;
+			return *this;
+		}
+		iterator& operator++ (int)const{
+			pointer tmp = this;
+			this->data++;
+			return tmp;
+		}
+		iterator& operator-- ()const{
+			this->data--;
+			return *this;
+		}
+
+		iterator& operator-- (int)const{
+
+			pointer tmp = this;
+			this->data--;
+			return tmp;
+		}
+		iterator& operator[](key)const{
+			return this->data+key;
+		}
+		iterator& operator == (iterator &comp) const{
+			return comp.data == this->data;
+		}
+		iterator& operator != (iterator &comp) const{
+			return comp.data != this->data;
+		}
+		iterator& operator < (iterator &comp) const{
+			return comp.data < this->data;
+		}
+		iterator& operator > (iterator &comp) const{
+			return comp.data > this->data;
+		}
+		iterator& operator >= (iterator &comp) const{
+			return comp.data > this->data;
+		}
+		iterator& operator <= (iterator &comp) const{
+			return comp.data > this->data;
+		}
+		iterator& operator + (iterator &comp) const{
+			return iterator(comp.data + this->data);
+		}
+		iterator& operator - (iterator &comp) const{
+			return iterator(comp.data - this->data);
+		}
+
+		iterator& operator += (iterator &comp) const{
+			return this->data  + comp.data;
+		}
+		iterator& operator -= (iterator &comp) const{
+			return this->data  - comp.data;
+		}
+
+		T& base(){
+			
+		}
+	}
 }
 
-iterators::~iterators()
-{
-}
 
-#endif
+#endif;
