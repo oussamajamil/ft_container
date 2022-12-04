dc96d3e254a8ba34141ac52387f0186ae604a67c
oussamajamil <oussamajamil01@gmail.com>
2022-12-04 11:21:22 +0100

    ft_container vector

--- main.cpp
+++ main.cpp
@@ -1,60 +1,434 @@
 #include "./vector/vector.hpp"
 #include <iostream>
 #include <vector>
+
+using std::cout;
+
+void vector_main(void)
+{
+
+    cout << "\033[0;32m** constructors **\033[0m\n";
+    {
+        ft::vector<int> defaultVect;
+        ft::vector<int> fillVect(4, 100);
+        ft::vector<int> rangeVect(fillVect.begin(), fillVect.end());
+        ft::vector<int> copyVect(rangeVect);
+
+        cout << "The contents of the vector are:";
+        for (ft::vector<int>::iterator it = copyVect.begin(); it != copyVect.end(); ++it)
+            cout << ' ' << *it;
+        cout << "\n";
+    }
+
+    cout << "\033[0;32m** operator= **\033[0m\n";
+    {
+        ft::vector<int> origin(3, 10);
+        ft::vector<int> copy(5, 20);
+
+        copy = origin;
+
+        cout << "The contents of the copy are:";
+        for (ft::vector<int>::iterator it = copy.begin(); it != copy.end(); ++it)
+            cout << ' ' << *it;
+        cout << "\n";
+    }
+
+    cout << "\033[0;32m** assign **\033[0m\n";
+    {
+        ft::vector<int> vect;
+        ft::vector<int> vect2;
+        vect.assign(5, 12);
+        vect2.assign(vect.begin(), vect.end() - 2);
+
+        cout << "The contents of the vector are: ";
+        for (size_t i = 0; i < vect2.size(); ++i)
+            cout << vect2[i] << ' ';
+        cout << '\n';
+    }
+
+    cout << "\033[0;32m** get_allocator **\033[0m\n";
+    {
+        ft::vector<int> vect;
+        int *ptr;
+
+        ptr = vect.get_allocator().allocate(5);
+
+        for (size_t i = 0; i < 5; ++i)
+            vect.get_allocator().construct(&ptr[i], i);
+
+        cout << "The allocated array contains:";
+        for (size_t i = 0; i < 5; ++i)
+            cout << ' ' << ptr[i];
+        cout << "\n";
+
+        for (size_t i = 0; i < 5; ++i)
+            vect.get_allocator().destroy(&ptr[i]);
+        vect.get_allocator().deallocate(ptr, 5);
+    }
+
+    cout << "\033[0;32m** at **\033[0m\n";
+    {
+        ft::vector<int> vect(10);
+
+        for (size_t i = 0; i < vect.size(); ++i)
+            vect.at(i) = i;
+
+        cout << "the vector contains:";
+        for (size_t i = 0; i < vect.size(); ++i)
+            cout << ' ' << vect.at(i);
+        cout << "\n";
+    }
+
+    cout << "\033[0;32m** operator[] **\033[0m\n";
+    {
+        ft::vector<int> vect(10);
+
+        for (size_t i = 0; i < vect.size(); ++i)
+            vect[i] = i;
+
+        cout << "the vector contains:";
+        for (size_t i = 0; i < vect.size(); ++i)
+            cout << ' ' << vect[i];
+        cout << "\n";
+    }
+
+    cout << "\033[0;32m** front **\033[0m\n";
+    {
+        ft::vector<int> vect;
+
+        vect.push_back(8);
+        vect.push_back(6);
+        cout << "the front of the vector is " << vect.front() << '\n';
+    }
+
+    cout << "\033[0;32m** back **\033[0m\n";
+    {
+        ft::vector<int> vect;
+        vect.push_back(10);
+        vect.push_back(12);
+        cout << "the back of the vector is " << vect.back() << '\n';
+    }
+
+    cout << "\033[0;32m** data **\033[0m\n";
+    {
+        ft::vector<int> vect(3, 10);
+
+        cout << "the vector contains:";
+        for (size_t i = 0; i < vect.size(); ++i)
+            cout << ' ' << vect.data()[i];
+        cout << '\n';
+    }
+
+    cout << "\033[0;32m** begin and end **\033[0m\n";
+    {
+        ft::vector<int> vect;
+
+        for (int i = 1; i <= 5; ++i)
+            vect.push_back(i);
+
+        cout << "the first element in the vector is: " << *vect.begin() << "\n";
+        cout << "the last element in the vector is: " << *(vect.end() - 1) << "\n";
+    }
+
+    // cout << "\033[0;32m** rbegin and rend **\033[0m\n";
+    // {
+    //     ft::vector<int> vect(5);
+    //     ft::vector<int>::iterator rit = vect.begin();
+    //     for (int i = 0; rit != vect.end(); ++rit, ++i)
+    //         *rit = i;
+    //     cout << "the vector elements in erverse order:";
+    //     for (ft::vector<int>::reverse_iterator it = vect.rbegin(); it != vect.rend(); ++it)
+    //         cout << ' ' << *it;
+    //     cout << "\n";
+    // }
+
+    cout << "\033[0;32m** empty **\033[0m\n";
+    {
+        ft::vector<int> vect;
+
+        cout << "before filling the vector, vect is empty: ";
+        cout << std::boolalpha << vect.empty();
+        cout << "\n";
+        for (int i = 0; i < 5; ++i)
+            vect.push_back(i);
+        cout << "after filling the vector, vect is empty: ";
+        cout << std::boolalpha << vect.empty();
+        cout << "\n";
+    }
+
+    cout << "\033[0;32m** size && max_size **\033[0m\n";
+    {
+        ft::vector<int> vect;
+        cout << "after creating a fresh vector, size: " << vect.size() << "\n";
+
+        for (int i = 0; i < 10; ++i)
+            vect.push_back(i);
+        cout << "after pushing 10 elements, size: " << vect.size() << "\n";
+
+        vect.insert(vect.end(), 10, 100);
+        cout << "after inserting 10 more elements, size: " << vect.size() << "\n";
+
+        vect.assign(12, 15);
+        cout << "after assigning 12 elements, size: " << vect.size() << "\n";
+
+        vect.pop_back();
+        cout << "after removing an element, size: " << vect.size() << "\n";
+
+        cout << "the max size of the vector is: " << vect.max_size() << '\n';
+    }
+
+    cout << "\033[0;32m** reserve **\033[0m\n";
+    {
+        ft::vector<int> vect(10, 10);
+        cout << "capacity before reserving anything: " << vect.capacity() << "\n";
+        vect.reserve(20);
+        cout << "capacity after reserving 20 elements: " << vect.capacity() << "\n";
+        vect.reserve(5);
+        cout << "capacity after reserving 5 elements: " << vect.capacity() << "\n";
+    }
+
+    cout << "\033[0;32m** capacity **\033[0m\n";
+    {
+        ft::vector<int> vect;
+
+        cout << "The progress of the capacity:";
+        for (int i = 0; i < 20; ++i)
+        {
+            vect.push_back(i);
+            cout << " " << (int)vect.capacity();
+        }
+        cout << "\n";
+    }
+
+    cout << "\033[0;32m** clear **\033[0m\n";
+    {
+        ft::vector<int> vect;
+        vect.push_back(10);
+        vect.push_back(20);
+        vect.push_back(30);
+
+        cout << "the vector contains:";
+        for (size_t i = 0; i < vect.size(); ++i)
+            cout << ' ' << vect[i];
+        cout << "\n";
+
+        vect.clear();
+        vect.push_back(3);
+        vect.push_back(17);
+
+        cout << "the vector now contains:";
+        for (size_t i = 0; i < vect.size(); ++i)
+            cout << ' ' << vect[i];
+        cout << "\n";
+    }
+
+    cout << "\033[0;32m** insert **\033[0m\n";
+    {
+        ft::vector<int> vect(5, 10);
+        ft::vector<int>::iterator it;
+
+        it = vect.insert(vect.begin(), 20);
+
+        vect.insert(it, 2, 30);
+        it = vect.begin();
+
+        ft::vector<int> vect2(2, 40);
+        vect.insert(it + 2, vect2.begin(), vect2.end());
+
+        cout << "the vector contains:";
+        for (it = vect.begin(); it < vect.end(); ++it)
+            cout << ' ' << *it;
+        cout << "\n";
+    }
+
+    cout << "\033[0;32m** erase **\033[0m\n";
+    {
+        ft::vector<int> vect;
+
+        for (int i = 1; i <= 10; ++i)
+            vect.push_back(i);
+
+        vect.erase(vect.begin() + 5);
+        vect.erase(vect.begin(), vect.begin() + 3);
+
+        cout << "the vector contains:";
+        for (size_t i = 0; i < vect.size(); ++i)
+            cout << ' ' << vect[i];
+        cout << "\n";
+    }
+
+    cout << "\033[0;32m** push_back **\033[0m\n";
+    {
+        ft::vector<int> vect;
+        for (size_t i = 1; i < 10; ++i)
+            vect.push_back(i * 10);
+        cout << "the vector contains: ";
+        for (size_t i = 0; i < vect.size(); ++i)
+            cout << vect[i] << " ";
+        cout << "\n";
+    }
+
+    cout << "\033[0;32m** pop_back **\033[0m\n";
+    {
+        ft::vector<int> vect;
+
+        for (size_t i = 0; i < 10; ++i)
+            vect.push_back(i);
+
+        vect.pop_back();
+        vect.pop_back();
+        vect.pop_back();
+
+        cout << "the vector contains: ";
+        for (size_t i = 0; i < vect.size(); ++i)
+            cout << vect[i] << " ";
+        cout << "\n";
+    }
+
+    cout << "\033[0;32m** resize **\033[0m\n";
+    {
+        ft::vector<int> vect;
+
+        for (int i = 1; i < 10; ++i)
+            vect.push_back(i);
+
+        vect.resize(5);
+        vect.resize(8, 100);
+        vect.resize(12);
+
+        cout << "the vector contains:";
+        for (size_t i = 0; i < vect.size(); ++i)
+            cout << ' ' << vect[i];
+        cout << "\n";
+    }
+
+    cout << "\033[0;32m** swap **\033[0m\n";
+    {
+        ft::vector<int> vect1(3, 10);
+        ft::vector<int> vect2(4, 20);
+
+        vect1.swap(vect2);
+
+        cout << "vect1 contains:";
+        for (size_t i = 0; i < vect1.size(); ++i)
+            cout << ' ' << vect1[i];
+        cout << "\n";
+
+        cout << "vect2 contains:";
+        for (size_t i = 0; i < vect2.size(); ++i)
+            cout << ' ' << vect2[i];
+        cout << "\n";
+    }
+
+    // cout << "\033[0;32m** relational operators **\033[0m\n";
+    // {
+    //     ft::vector<int> vect1(3, 20);
+    //     ft::vector<int> vect2(4, 10);
+
+    //     if (vect1 == vect2)
+    //         cout << "vect1 and vect2 are equal\n";
+    //     if (vect1 != vect2)
+    //         cout << "vect1 and vect2 are not equal\n";
+    //     if (vect1 < vect2)
+    //         cout << "vect1 is less than vect2\n";
+    //     if (vect1 > vect2)
+    //         cout << "vect1 is greater than vect2\n";
+    //     if (vect1 <= vect2)
+    //         cout << "vect1 is less than or equal to vect2\n";
+    //     if (vect1 >= vect2)
+    //         cout << "vect1 is greater than or equal to vect2\n";
+    // }
+
+    cout << "\033[0;32m** swap (non-member) **\033[0m\n";
+    {
+        ft::vector<int> vect1(3, 10);
+        ft::vector<int> vect2(4, 20);
+
+        swap(vect1, vect2);
+
+        cout << "vect1 contains:";
+        for (size_t i = 0; i < vect1.size(); ++i)
+            cout << ' ' << vect1[i];
+        cout << "\n";
+
+        cout << "vect2 contains:";
+        for (size_t i = 0; i < vect2.size(); ++i)
+            cout << ' ' << vect2[i];
+        cout << "\n";
+    }
+}
+
 int main_t()
 {
-    ft::vector<int> a;
-    ft::vector<int>::iterator ptr;
 
-    // ft::vector<int> c(5, 4);
+    ft::vector<int> vec;
+    vec.push_back(1);
+    vec.push_back(2);
+    vec.push_back(3);
+    vec.push_back(4);
+    vec.push_back(5);
+    vec.push_back(6);
+    vec.push_back(7);
+    vec.push_back(8);
+    vec.push_back(9);
 
-    // a.assign(5, 10);
-    // c.assign(a.begin() + 2, a.end());
-    a.push_back(0);
-    a.push_back(1);
-    a.push_back(2);
-    a.push_back(3);
-    a.push_back(4);
-    a.push_back(5);
-    a.push_back(6);
-    a.push_back(7);
-
-    a.erase(a.begin() + 2, a.begin() + 5);
-    // std::cout << a.size() << "--";
-    // ft::vector<int>::iterator it;
-    // std::cout << a.size() << std::endl;
-    for (std::size_t i = 0; i < a.size(); i++)
+    for (size_t i = 0; i < vec.size(); i++)
     {
-        std::cout << a[i] << " ";
+        std::cout << vec.at(i) << std::endl;
     }
-    // a.push_back(8);
-    // a.push_back(9);
-    // std::cout<< a.capacity() << std::endl;
-    // a.reserve(20);
-    // std::cout << a.capacity() << std::endl;
-    // a.assign(5, 10);
-    // a.reserve(3);
-    // a.push_back(20);
-    // a.push_back(10);
-    // a.push_back(30);
-    // std::cout << a.size() << std::endl;
-    // for (int i = 0; i < a.size(); i++)
+
+    // std::cout << vec.at(4) << "-------";
+
+    // ft::vector<ft::vector<int> > vect(5, vectInt);
+
+    // std::vector<int> a(10, 5);
+    // ft::vector<int> b(10, 5);
+    // ft::vector<int> b(10, 4);
+    // ft::vector<int>::iterator ptr;
+    // a.push_back(0);
+    // a.push_back(1);
+    // a.push_back(2);
+    // a.push_back(3);
+    // a.push_back(4);
+    // a.push_back(5);
+    // a.push_back(6);
+    // a.push_back(7);
+    // a.swap(b);
+    // for (std::size_t i = 0; i < b.size(); i++)
     // {
-    //     std::cout << a[i] << " ";
+    //     std::cout << b[i] << " ";
     // }
-    // std::cout << "\n"
-    //           << std::endl;
+    // return 0;
 
-    // std::cout << a.front() << std::endl;
-    // std::cout << a.back() << std::endl;
+    // a.clear();
+    // std::cout << a.size() << "||" << a.capacity();
 
+    // b.clear();
+    // std::cout << b.size() << "||" << b.capacity();
     return 0;
 }
 
 int main()
 {
 
-    main_t();
+    cout << "\033[0;32m** resize **\033[0m\n";
+    {
+        ft::vector<int> vect;
+
+        for (int i = 0; i < 10; ++i)
+            vect.push_back(i);
+
+        vect.resize(5);
+        // vect.resize(8, 100);
+        // vect.resize(12);
+
+        cout << "the vector contains:";
+        for (size_t i = 0; i < vect.size(); ++i)
+            cout << ' ' << vect[i];
+        cout << "\n";
+    }
+    // vector_main();
+    // main_t();
     // system("leaks a.out");
 
     return 0;
--- vector/vector.hpp
+++ vector/vector.hpp
@@ -34,19 +34,21 @@ namespace ft
             this->_alloc = alloc;
             this->_size = 0;
             this->_capacity = 0;
-            this->_data = nullptr;
+            this->_data = NULL;
         }
+
         explicit vector(size_type n, const value_type &val = value_type(), const allocator_type &alloc = allocator_type())
         {
+            this->_size = n;
             this->_capacity = n;
             this->_alloc = alloc;
-            this->_size = n;
-            this->_data = _alloc.allocate(_capacity);
-            for (std::size_t i = 0; i < _capacity; i++)
+            this->_data = _alloc.allocate(n);
+            for (std::size_t i = 0; i < n; i++)
             {
                 this->_data[i] = val;
             }
         }
+
         template <class InputIterator>
         vector(InputIterator first, InputIterator last, const allocator_type &alloc = allocator_type(),
                typename ft::enable_if<!ft::is_integral<InputIterator>::value, InputIterator>::type * = 0)
@@ -79,9 +81,12 @@ namespace ft
             {
                 this->_alloc.destroy(this->_data + i);
             }
-            if (this->capacity > 0)
+            if (this->capacity() > 0)
                 this->_alloc.deallocate(this->_data, this->_capacity);
+            _size = x._size;
+            _capacity = x._capacity;
             this->_data = this->_alloc.allocate(_capacity);
+
             for (size_t i = 0; i < x._size; ++i)
             {
                 this->_data[i] = x._data[i];
@@ -99,15 +104,12 @@ namespace ft
         }
         ~vector()
         {
-            for (int i = 0; i < _size; i++)
+            for (size_t i = 0; i < _size; ++i)
             {
                 _alloc.destroy(_data + i);
             }
             if (this->_capacity)
                 _alloc.deallocate(_data, _capacity);
-            _size = 0;
-            _capacity = 0;
-            _data = NULL;
         }
         iterator begin()
         {
@@ -134,33 +136,42 @@ namespace ft
         {
             return const_reverse_iterator(end());
         }
+        reverse_iterator rend()
+        {
+            return reverse_iterator(begin());
+        }
+        const_reverse_iterator rend() const
+        {
+            return const_reverse_iterator(begin());
+        }
         size_type capacity() const
         {
             return _capacity;
         }
         bool empty() const
         {
-            _size ? true : false;
+            return !_size ? true : false;
         }
         size_type max_size() const
         {
             return this->_alloc.max_size();
         }
+
         void reserve(size_type n)
         {
             if (n > _alloc.max_size())
                 throw std::length_error("param > max_size");
             else if (n > _capacity)
             {
-                pointer new_dt = _alloc.allocate(_capacity);
+                pointer new_dt = _alloc.allocate(n);
                 for (size_type i = 0; i < _size; i++)
-                    _alloc.construct(new_dt + i, _data[i]);
+                    new_dt[i] = _data[i];
                 for (size_type i = 0; i < _size; i++)
                     _alloc.destroy(_data + i);
                 if (_capacity > 0)
                     _alloc.deallocate(_data, _size);
                 _capacity = n;
-                _data = new_dt;
+                std::swap(_data, new_dt);
             }
         }
 
@@ -176,9 +187,9 @@ namespace ft
 
         void pop_back()
         {
-            if (_size > 0)
+            if (_size >= 0)
             {
-                _alloc.destroy(_data + _size - 1);
+                _alloc.destroy(_data + _size);
                 _size--;
             }
         }
@@ -243,8 +254,7 @@ namespace ft
         iterator insert(iterator position, const value_type &val)
         {
             size_t k = 0;
-            ft::vector<value_type>
-                tmp;
+            ft::vector<value_type> tmp;
             tmp.assign(position, end());
             _size = position - begin();
             if (_size + 1 > _capacity)
@@ -262,8 +272,25 @@ namespace ft
             _size += k;
             return position;
         }
+
+        void insert(iterator position, size_type n, const value_type &val)
+        {
+            difference_type m = end() - position;
+            ft::vector<value_type> tmp;
+            tmp.assign(position, end());
+            for (size_t i = position - begin(); i < _size; i++)
+                _alloc.destroy(_data + i);
+            if (_size + n > _capacity)
+                reserve(_size + m);
+            _size -= m;
+            for (size_t i = 0; i < n; i++)
+                push_back(val);
+            for (size_t i = 0; i < tmp.size(); i++)
+                push_back(tmp[i]);
+        }
         template <class InputIterator>
-        void insert(iterator position, InputIterator first, InputIterator last)
+        void insert(iterator position, InputIterator first, InputIterator last,
+                    typename ft::enable_if<!ft::is_integral<InputIterator>::value, InputIterator>::type * = 0)
         {
             difference_type n = end() - position;
             difference_type m = last - first;
@@ -274,7 +301,7 @@ namespace ft
             if (_size + m > _capacity)
                 reserve(_size + m);
             _size -= n;
-            for (iterator it = first; it != last; it++)
+            for (InputIterator it = first; it != last; it++)
             {
                 push_back(*it);
             }
@@ -283,6 +310,7 @@ namespace ft
                 push_back(tmp[k]);
             }
         }
+
         iterator erase(iterator position)
         {
             size_t k = 0;
@@ -296,6 +324,7 @@ namespace ft
             _size += tmp.size();
             return position;
         }
+
         iterator erase(iterator first, iterator last)
         {
             ft::vector<value_type> tmp(last, end());
@@ -308,9 +337,54 @@ namespace ft
             return it;
         }
 
-        void swap(vector &x){
-            
+        void swap(vector &x)
+        {
+            size_t tmp_size = x.size();
+            size_t capacity = x.capacity();
+            pointer res = x._data;
+            x._size = _size;
+            _size = tmp_size;
+            x._capacity = _capacity;
+            _capacity = capacity;
+            x._data = _data;
+            _data = res;
         }
+        allocator_type get_allocator() const
+        {
+            return _alloc;
+        }
+        value_type *data()
+        {
+            return _data;
+        }
+        const value_type *data() const
+        {
+            return _data;
+        }
+        void clear()
+        {
+            for (size_t i = 0; i < _size; i++)
+                _alloc.destroy(_data + i);
+            _size = 0;
+        }
+        reference at(size_type n)
+        {
+            if (n > _size)
+                throw std::out_of_range("posotion superieure size");
+            return _data[n];
+        }
+
+        void resize(size_type n, value_type val = value_type())
+        {
+
+            if (_size > n)
+                for (size_t i = 0; i <= (_size - n +2) ; ++i)
+                    pop_back();
+            // else if (_size < n)
+            //     for (size_t i = 0; i < n - _size; ++i)
+            //         push_back(val);
+        }
+
     private:
         pointer _data;
         size_t _size;
